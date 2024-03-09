fc9863305233e52f50abdc27a0e6418687c55cc6
Agunbiade Adedeji <dejongbaba@gmail.com>
2024-03-09 19:05:24 +0100

    fix: add styling fix to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,59 +1,85 @@
 'use client'
 import React from 'react';
-import {useScroll} from "framer-motion";
+import {motion} from "framer-motion";
 
-// import {useScroll} from 'framer-motion';
 function Footer() {
-    const scroll = useScroll()
     return (
-        <div className='bg-black p-8 relative bottom'>
-            <div className=" divide-y ">
-                <div className='flex py-6 justify-between items-start'>
-                    <ul className='flex space-x-2'>
-                        <li><a href="#" className=' hover:text-white text-gray-300 transition '>About</a></li>
-                        <li><a href="#" className=' hover:text-white text-gray-300 transition '>Why us</a></li>
-                        <li><a href="#" className=' hover:text-white text-gray-300 transition '>Platform</a></li>
-                        <li><a href="#" className=' hover:text-white text-gray-300 transition '>Pricing</a></li>
-                        <li><a href="#" className=' hover:text-white text-gray-300 transition '>Contact</a></li>
+        <motion.div className='bg-black px-[30px] py-[40px] relative bottom'>
+            <div className=" divide-y divide-gray-100 space-y-8">
+                <motion.div
+                    whileInView={{opacity: 1, scale: 1, translateY: 0}}
+                    viewport={{once: true}}
+                    initial={{opacity: 0, scale: 0, translateY: -100}}
+                    transition={
+                        {opacity: {duration: 0.4}, delay: 0.5}
+                    }
+                    className='flex py-6 justify-between items-center'>
+                    <ul className='flex font-light'>
+                        <li><a href="#" className='hover:text-white text-gray-300 transition '>About</a></li>
+                        <li><a href="#" className='mx-8 hover:text-white text-gray-300 transition '>Why us</a></li>
+                        <li><a href="#" className='mx-8 hover:text-white text-gray-300 transition '>Platform</a></li>
+                        <li><a href="#" className='mx-8 hover:text-white text-gray-300 transition '>Pricing</a></li>
+                        <li><a href="#" className='mx-8 hover:text-white text-gray-300 transition '>Contact</a></li>
                     </ul>
                     <h3 className='text-5xl text-white'>hello@ramos.com</h3>
-                </div>
-                <div className='grid grid-cols-4 py-6 '>
+                </motion.div>
+                <motion.div
+                    whileInView={{opacity: 1, scale: 1, translateY: 0}}
+                    viewport={{once: true}}
+                    initial={{opacity: 0, scale: 0, translateY: -100}}
+                    transition={
+                        {opacity: {duration: 0.4}, delay: 0.7}
+                    }
+                    className='grid grid-cols-4 py-[50px] '>
                     <div className='col-span-1'>
                         <h3 className='text-white'>Warrensville Heights</h3>
-                        <p className='text-xs text-gray-300'>14418 Vineyard Drive, NC</p>
-                        <p className='text-xs text-gray-300'>44128</p>
+                        <p className=' text-gray-300 font-thin'>14418 Vineyard Drive, NC</p>
+                        <p className=' text-gray-300 font-thin'>44128</p>
                     </div>
                     <div className='col-span-2'>
-                        <h3 className='text-white'>Saint Louis </h3>
-                        <p className='text-xs text-gray-300'>1366 Penn Street</p>
-                        <p className='text-xs text-gray-300'>63101</p>
+                        <h3 className='text-white font-normal'>Saint Louis </h3>
+                        <p className=' text-gray-300 font-thin'>1366 Penn Street</p>
+                        <p className=' text-gray-300 font-thin'>63101</p>
                     </div>
-                    <ul className='col-span-1 space-y-3 text-right'>
-                        <li className='text-white font-normal text-xl'>Linkedin</li>
-                        <li className='text-white font-normal text-xl'>Instagram</li>
-                        <li className='text-white font-normal text-xl'>Facebook</li>
+                    <ul className='col-span-1 space-y-3 text-right font-thin '>
+                        <li className='text-white  text-xl'>Linkedin</li>
+                        <li className='text-white  text-xl'>Instagram</li>
+                        <li className='text-white  text-xl'>Facebook</li>
                     </ul>
-                </div>
+                </motion.div>
             </div>
             <ul className='flex items-center space-x-4 justify-between'>
                 <li>
-                    <p className='text-9xl text-white'>Ramos<span>&reg;</span></p>
+                    <motion.p
+                        whileInView={{opacity: 1, scale: 1, translateY: 0}}
+                        viewport={{once: true}}
+                        initial={{opacity: 0, scale: 0, translateY: -100}}
+                        transition={
+                            {opacity: {duration: 0.4}, delay: 0.9}
+                        }
+                        className='text-9xl text-white'>Ramos<span>&reg;</span></motion.p>
                     {/*<img className='w-[400px]' src="/images/png/ramos-registered.png" alt="ramos registered logo"/>*/}
                 </li>
-                <li className='text-gray-300'>
+                <li className='text-gray-300 font-thin'>
                     Privacy
                 </li>
-                <li className='text-gray-300'>
+                <li className='text-gray-300 font-thin'>
                     License agreement
                 </li>
                 <li>
-                    <img className='w-[100px]' src="/images/png/barcode.png" alt="barcode"/>
+                    <motion.img
+                        whileInView={{opacity: 1, scale: 1}}
+                        viewport={{once: true}}
+                        initial={{opacity: 0, scale: 0}}
+                        transition={
+                            {opacity: {duration: 0.4}, delay: 0.9}
+                        }
+                        className='w-[100px]' src="/images/png/barcode.png" alt="barcode"/>
                 </li>
             </ul>
 
 
-        </div>
+        </motion.div>
     );
 }
 
